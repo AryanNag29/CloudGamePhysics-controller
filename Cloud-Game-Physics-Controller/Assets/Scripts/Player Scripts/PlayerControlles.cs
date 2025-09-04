e9d975b9a6e15da0b03d42c3a26b989fb69a265d
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-09-04 17:42:20 +0530

    Made basic structure for Player Controller script

---
+++ Cloud-Game-Physics-Controller/Assets/Scripts/Player Scripts/PlayerControlles.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerControlles : MonoBehaviour
+{
+    #region variables
+
+    //movement variables
+
+    #endregion
+
+
+    #region componentReference
+    //rigidbody component
+    [SerializeField] Rigidbody rb;
+
+    #endregion
+
+
+    #region Awake
+
+    void Awake()
+    {
+
+    }
+    #endregion
+
+
+    #region Update
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+}
